612a82b54cc0b9879deae4bd3bfa96728935de82
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2021-12-10 21:13:56 +0800

    Command introduction

---
+++ PLDWhileAssign01.py
@@ -0,0 +1,8 @@
+# The application of lesson to the lottery program
+# Mechanics: The program will generate random number 3 from 0 to 100 then show it
+# The program will ask to run until the 3 values are the same to show Winner for the user
+
+
+#Making command to continue
+print("To run the lottery press ""Y"" or ""N"" to exit the program")  
+
