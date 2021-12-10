3bb57c5b9de12fc2c23697e423321c84ba960cc4
JohnRen1211 <johnrengelojaylo@iskolarngbayan.pup.edu.ph>
2021-12-10 21:18:08 +0800

    For introduction

---
+++ PLDWhileAssign02.py
@@ -0,0 +1,15 @@
+# The application of lesson to guessing program
+# Mechanics: The program will generate random number from 0 to 100 then the user will guess
+# If the given value of the user is less than the generated value print less than
+# If the given value of the user is greater than the generate value print greater than
+# If the given value of the user is the same with the hidden value print guessed correctly
+
+
+# Making command to continue
+print("To run the guessing program press ""Y"" to continue or ""N"" to exit the program")  
+
+
+
+
+
+
